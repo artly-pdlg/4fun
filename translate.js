8ad750fe75490460306e41164b3f637742f7e5db
artly-pdlg <72287382+artly-pdlg@users.noreply.github.com>
2021-10-20 20:44:25 +0800

    Add files via upload

--- translate.js
+++ translate.js
@@ -7,8 +7,8 @@ function translate(s) {
     tree = buildblock(words, 0)[0];
     console.log(tree);
     code = [];
-    translatetree(tree, 0,code);
+    translatetree(tree, 0, code);
     code.push('end');
-    //for(i in code)code[i]=i+': '+code[i];
-   return code.join('\n');
+    if (islinenumber) for (i in code) code[i] = i + ': ' + code[i];
+    return code.join('\n');
 }
